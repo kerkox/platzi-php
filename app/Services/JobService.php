3e3ac53e3d7e703177c0a640099fd07f207cbaba
Paul Cortes <paulkerg@gmail.com>
2020-04-14 23:11:16 -0500

    improvement over JobService

--- app/Services/JobService.php
+++ app/Services/JobService.php
@@ -7,7 +7,7 @@ use App\Models\Job;
 class JobService
 {
     public function deleteJob($id) {
-        $job = Job::find($id);
+        $job = Job::findOrFail($id);
         $job->delete();
     }
 }
