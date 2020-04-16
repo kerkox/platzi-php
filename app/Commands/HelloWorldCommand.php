f4536fa362eb2dfa72ef2836a1fcd3ae72379ac8
Paul Cortes <paulkerg@gmail.com>
2020-04-16 00:06:49 -0500

    primer comando hello world

--- app/Commands/HelloWorldCommand.php
+++ app/Commands/HelloWorldCommand.php
@@ -1,10 +1,23 @@
 <?php
-
-
 namespace App\Commands;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class HelloWorldCommand
+class HelloWorldCommand extends Command
 {
+    protected static $defaultName = 'app:hello-world';
 
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        echo 'Hello World';
+
+        return 0;
+    }
 }
--- console.php
+++ console.php
@@ -2,10 +2,11 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
+use App\Commands\HelloWorldCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 // ... register commands
-
+$application->add(new HelloWorldCommand());
 $application->run();
