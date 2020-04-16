3e82cb71d72a202f778695bcb614492ae98819f1
Paul Cortes <paulkerg@gmail.com>
2020-04-16 00:11:36 -0500

    ajustes para recibir parametros en los comandos

--- app/Commands/HelloWorldCommand.php
+++ app/Commands/HelloWorldCommand.php
@@ -2,6 +2,7 @@
 namespace App\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,12 +12,12 @@ class HelloWorldCommand extends Command
 
     protected function configure()
     {
-        // ...
+        $this->addArgument('name', InputArgument::REQUIRED, 'The username of the user.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello World');
+        $output->writeln('Hello '.$input->getArgument('name'));
         return 0;
     }
 }
